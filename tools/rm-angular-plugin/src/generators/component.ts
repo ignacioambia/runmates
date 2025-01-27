f2cafc5547175760d835740d7fd7cbcb0413da7e
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-01-27 01:16:08 -0600

    feat(rm-angular-plugin): add recently added component to index.ts file

--- tools/rm-angular-plugin/src/generators/component.ts
+++ tools/rm-angular-plugin/src/generators/component.ts
@@ -3,11 +3,12 @@ import {
   generateFiles,
   getProjects,
   Tree,
-  
 } from '@nx/devkit';
 import * as path from 'path';
 import { ComponentGeneratorSchema } from './schema';
 
+//TODO: Find nearest index.ts and export info there
+//TODO: Allow model.ts creation within the component
 export async function componentGenerator(
   tree: Tree,
   options: ComponentGeneratorSchema
@@ -22,19 +23,30 @@ export async function componentGenerator(
   const projects = getProjects(tree);
   const chosenProject = projects.get(options.project);
   const projectRoot = `${chosenProject.sourceRoot}/${options.path}`;
-
-  if (tree.exists(projectRoot)) {
-    throw new Error(`Component ${options.name} already exists at ${projectRoot}`);
-  }
-
+  const splittedPath = options.path.split('/');
 
   options.prefix = chosenProject['prefix'];
-  const splittedPath = options.path.split('/')
   options.name = splittedPath.pop();
   options.componentName = toCamelCase(options.prefix) + toCamelCase(options.name);
   options.storybookTitle = `${splittedPath.join('/')}/${options.componentName}`;
 
+  if (tree.exists(projectRoot)) {
+    throw new Error(`Component "${options.name}" already exists at ${projectRoot}`);
+  }
+
   generateFiles(tree, path.join(__dirname, 'files'), projectRoot, options);
+
+    const indexPath = path.join(chosenProject.sourceRoot, 'index.ts');
+    const exportStatement = `export * from './${options.path}/${options.name}.component';`;
+  
+    //only if index.ts file was found edit it, otherwise don't
+    if (tree.exists(indexPath)) {
+      const content = tree.read(indexPath, 'utf-8');
+      if (!content.includes(exportStatement)) {
+        tree.write(indexPath, `${content.trim()}\n${exportStatement}\n`);
+      }
+    }
+
   await formatFiles(tree);
 }
 
