a4f118ea3aaab19e78c9d7746232df33ac3e4bad
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-01-26 19:45:42 -0600

    chore(landing-page): configure landing page

--- apps/landing-page/src/app/app.component.ts
+++ apps/landing-page/src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
-
 @Component({
-  imports: [NxWelcomeComponent, RouterModule],
-  selector: 'app-root',
+  imports: [RouterModule],
+  selector: 'ld-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
