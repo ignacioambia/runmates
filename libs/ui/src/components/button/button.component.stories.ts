6ae2c83a88c8a29ea2d3622d2d6c5b9bdf3ccf33
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-02-02 21:22:49 -0600

    feat(ui button): add disabled funcitonality

--- libs/ui/src/components/button/button.component.stories.ts
+++ libs/ui/src/components/button/button.component.stories.ts
@@ -10,7 +10,14 @@ type Story = StoryObj<RmButton>;
 
 export const Primary: Story = {
   args: {},
-  render: (args) => ({
+  render: () => ({
     template: '<button rm-button>Click me</button>'
+  })
+};
+
+export const Disabled: Story = {
+  args: {},
+  render: () => ({
+    template: '<button rm-button [disabled]="true">Click me</button>'
   })
 };
