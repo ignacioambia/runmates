6d3d81f6bc3ffeb5f9848f6f93a1d1c5dd6a7e41
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-02-13 23:29:18 -0600

    feat(runmates-back): add lead resource

---
+++ apps/runmates-back/src/app/lead/lead.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { LeadService } from './lead.service';
+import { LeadController } from './lead.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Lead, LeadSchema } from './schema/lead.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{name: Lead.name, schema: LeadSchema }])],
+  controllers: [LeadController],
+  providers: [LeadService],
+})
+export class LeadModule {}
