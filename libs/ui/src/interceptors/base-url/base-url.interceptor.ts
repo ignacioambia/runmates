f4590a200b6cdf7f77e74b52a3166808b54e12e3
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-02-16 13:33:42 -0600

    fix(ui): remove unnecessary code

--- libs/ui/src/interceptors/base-url/base-url.interceptor.ts
+++ libs/ui/src/interceptors/base-url/base-url.interceptor.ts
@@ -2,7 +2,6 @@ import { HttpInterceptorFn } from '@angular/common/http';
 
 export const baseUrlInterceptor: HttpInterceptorFn = (req, next) => {
   const baseUrl = 'http://localhost:3000';
-  console.log('Calling baseUrl interceptor');
   const apiReq = req.clone({url: `${baseUrl}${req.url}`})
   return next(apiReq);
 };
