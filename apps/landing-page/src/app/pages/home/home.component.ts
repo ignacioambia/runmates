8027134de1c124faef6d6338cb1bd40235c9c0be
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-02-16 12:52:05 -0600

    feat(home): send new lead

--- apps/landing-page/src/app/pages/home/home.component.ts
+++ apps/landing-page/src/app/pages/home/home.component.ts
@@ -6,25 +6,44 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { RmButton, RmInput } from '@runmates/ui';
+import { RmButton, RmDialogService, RmInput, RmPartyIcon } from '@runmates/ui';
 import { LdCoundown } from './coundown/coundown.component';
+import { HttpClient } from '@angular/common/http';
+import { finalize, Subscription } from 'rxjs';
 
 @Component({
   selector: 'ld-home',
   imports: [CommonModule, ReactiveFormsModule, RmInput, RmButton, LdCoundown],
   templateUrl: './home.component.html',
+  providers: [RmDialogService],
   styleUrl: './home.component.scss',
 })
 export class LdHome {
-  public validForm = false;
+  public validForm = true;
+  public isSubmitting = false;
   public infoFg = new FormGroup({
     name: new FormControl(''),
     email: new FormControl('', [Validators.required, Validators.email]),
   });
 
-  constructor() {
+  constructor(private http: HttpClient, private dialog: RmDialogService) {
     this.infoFg.statusChanges.subscribe((status) => {
       this.validForm = status === 'VALID';
     });
+  }
+
+  public submitLead(): Subscription {
+    this.isSubmitting = true;
+    return this.http
+      .post('/lead', this.infoFg.value)
+      .pipe(finalize(() => (this.isSubmitting = false)))
+      .subscribe(() => {
+        this.dialog.open({
+          title: '¡Estás dentro!',
+          content:
+            'Te enviaremos un correo en cuanto la aplicación esté disponible.',
+          icon: RmPartyIcon,
+        });
+      });
   }
 }
