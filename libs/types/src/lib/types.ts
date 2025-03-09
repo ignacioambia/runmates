73cfd20f2934423b2066b20aab2e0b9bfd66d6de
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-03-09 10:31:01 -0600

    chore(types): add weekdays type

--- libs/types/src/lib/types.ts
+++ libs/types/src/lib/types.ts
@@ -1,11 +1,11 @@
 export type TrainingStatus = "completed" | "skippped";
 export type TrainingIntensity = "rest" | "low" | "medium" | "high";
-
+export type WeekDay = 'Monday' | 'Tuesday'  | 'Wednesday' | 'Thursday' | 'Friday' | 'Saturday' | 'Sunday';
 export interface Training {
   activities: TrainingActivity[];
   notes: string;
   intensity: TrainingIntensity;
-  weekday: string
+  weekday: WeekDay
 }
 
 export interface TrainingActivity {
