cd3e116343b9f8a1cfc4239b714cf90b42b580d2
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-03-08 19:06:02 -0600

    chore(ui icons): update imports

--- libs/ui/src/components/index.ts
+++ libs/ui/src/components/index.ts
@@ -1,5 +1,4 @@
 export * from './button/button.component';
-export * from './icons';
 export * from './feedback';
 export * from './progress-bar/progress-bar.component';
 export * from './training-card/training-card.component';
