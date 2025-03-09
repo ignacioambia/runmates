25081bf0ffcd2616517dd82b3e9be28a2b8be1b0
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-03-08 19:48:50 -0600

    feat(ui training-card): Add multiple activities to a card

--- libs/ui/src/components/training-card/training-card.component.stories.ts
+++ libs/ui/src/components/training-card/training-card.component.stories.ts
@@ -1,12 +1,17 @@
-import { componentWrapperDecorator, type Meta, type StoryObj } from '@storybook/angular';
+import { trainingWithThreeSeesions } from './@mocks/trainings';
+import {
+  argsToTemplate,
+  componentWrapperDecorator,
+  type Meta,
+  type StoryObj,
+} from '@storybook/angular';
 import { RmTrainingCard } from './training-card.component';
 
 export default {
   component: RmTrainingCard,
   title: 'components/RmTrainingCard',
   decorators: [
-    componentWrapperDecorator((story) => `<div style="width: 170px;">${story}</div>`)
-  ]
+  ],
 } as Meta<RmTrainingCard>;
 
 type Story = StoryObj<RmTrainingCard>;
@@ -26,4 +31,55 @@ export const Single: Story = {
       weekday: 'Monday',
     },
   },
+  render: (args) => ({
+    props: args,
+    template: `
+    <div style="width: 170px;">
+      <rm-training-card ${argsToTemplate(args)}/>
+    </div>
+    `,
+  }),
+};
+
+export const Double: Story = {
+  args: {
+    training: {
+      activities: [
+        {
+          title: 'Easy Run',
+          distance: 5,
+        },
+        {
+          title: 'Strides',
+          complementary_training: '4X20s',
+        },
+      ],
+      intensity: 'medium',
+      notes:
+        'Run easy but end with high-effort strides to build speed and agility. Perform four 20-second strides at near max effort. Keep a comfortable pace throughout your run.',
+      weekday: 'Wednesday',
+    },
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <div style="width: 352px;">
+      <rm-training-card ${argsToTemplate(args)}/>
+    </div>
+    `,
+  }),
+};
+
+export const Triple: Story = {
+  args: {
+    training: trainingWithThreeSeesions,
+  },
+  render: (args) => ({
+    props: args,
+    template: `
+    <div style="width: 352px;">
+      <rm-training-card ${argsToTemplate(args)}/>
+    </div>
+    `,
+  }),
 };
