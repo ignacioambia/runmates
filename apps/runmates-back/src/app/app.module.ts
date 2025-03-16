e3e3cdecf377b3c00c5716e1934b0b8fe81f49fe
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-03-16 09:47:56 -0600

    chore(runmates-back): use postgres instead of mongodb

--- apps/runmates-back/src/app/app.module.ts
+++ apps/runmates-back/src/app/app.module.ts
@@ -2,18 +2,26 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule, ConfigService } from '@nestjs/config';
-import { MongooseModule } from '@nestjs/mongoose';
 import { LeadModule } from './lead/lead.module';
 import { TrainingPlanModule } from './training-plan/training-plan.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
-    MongooseModule.forRootAsync({
-      inject: [ConfigService],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
       useFactory: async (configService: ConfigService) => ({
-        uri: configService.get<string>('MONGO_URI'),
+        type: 'postgres',
+        host: configService.get<string>('POSTGRES_HOST'),
+        port: configService.get<number>('POSTGRES_PORT'),
+        username: configService.get<string>('POSTGRES_USER'),
+        password: configService.get<string>('POSTGRES_PASSWORD'),
+        database: configService.get<string>('POSTGRES_DB'),
+        autoLoadEntities: true,
+        synchronize: false, // Set to false in production
       }),
+      inject: [ConfigService],
     }),
     LeadModule,
     TrainingPlanModule,
--- apps/runmates-back/src/app/lead/entities/lead.entity.ts
+++ apps/runmates-back/src/app/lead/entities/lead.entity.ts
@@ -1,1 +1,13 @@
-export class Lead {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Lead {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+}
