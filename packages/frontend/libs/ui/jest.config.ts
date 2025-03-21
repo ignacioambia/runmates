55312de67c9074e76e82e5aaa5904ca581cf239a
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-03-20 21:30:10 -0600

    chore(ui): update router after moving to frontend folder

--- packages/frontend/libs/ui/jest.config.ts
+++ packages/frontend/libs/ui/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'ui',
-  preset: '../../jest.preset.js',
+  preset: '../../../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
   coverageDirectory: '../../coverage/libs/ui',
   transform: {
