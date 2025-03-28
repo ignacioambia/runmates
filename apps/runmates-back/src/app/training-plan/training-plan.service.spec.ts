c04558b4a30d9303cef903cbb8516d887245b6ff
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-03-27 21:00:09 -0600

    chore(runmates-back): update tests to use mock repositories for Lead and TrainingPlan entities

--- apps/runmates-back/src/app/training-plan/training-plan.service.spec.ts
+++ apps/runmates-back/src/app/training-plan/training-plan.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TrainingPlanService } from './training-plan.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { mockRepository } from '../../test-utils/mock-repository.util';
+import { TrainingPlan } from './entities/training-plan.entity';
 
 describe('TrainingPlanService', () => {
   let service: TrainingPlanService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TrainingPlanService],
+      providers: [TrainingPlanService,
+        {
+          provide: getRepositoryToken(TrainingPlan),
+          useValue: mockRepository,
+        }
+      ],
     }).compile();
 
     service = module.get<TrainingPlanService>(TrainingPlanService);
