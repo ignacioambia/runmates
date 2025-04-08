316e92d27cef8a1e88b7ce3acfb4f138ba5a1dab
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-04-07 22:50:50 -0600

    feat(chat-input): enhance chat input component with textarea and auto-resize functionality

--- libs/ui/src/inputs/chat-input/chat-input.component.stories.ts
+++ libs/ui/src/inputs/chat-input/chat-input.component.stories.ts
@@ -1,19 +1,44 @@
 import type { Meta, StoryObj } from '@storybook/angular';
 import { RmChatInput } from './chat-input.component';
 import { fn } from '@storybook/test';
+import { withActions } from '@storybook/addon-actions/decorator';
 
 export default {
   component: RmChatInput,
   title: 'inputs/RmChatInput',
-  args: {
-    sendMessage: fn(),
-  }
+  argTypes: {
+    messageSent: {
+      action: 'messageSent',
+    },
+  },
 } as Meta<RmChatInput>;
 
 type Story = StoryObj<RmChatInput>;
 
-export const Primary: Story = {
+export const Empty: Story = {
   args: {
-    placeholder: 'Hello world',
-  }
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    message: 'Hello world',
+  },
+};
+
+export const ManyLines: Story = {
+  args: {
+    message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+  },
+  decorators: [
+    (storyFunc) => {
+      const story = storyFunc();
+      setTimeout(() => {
+        const textarea = document.querySelector('textarea');
+        textarea?.dispatchEvent(new Event('input', { bubbles: true }));
+        textarea?.focus();
+      });
+      return story;
+    },
+  ],
 };
