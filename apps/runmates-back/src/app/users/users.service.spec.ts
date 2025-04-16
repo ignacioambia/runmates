a30b49512e7a0b615eaedde9dac3a869c7d211b4
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-04-15 21:44:45 -0600

    test(auth, users): add JwtService to AuthController and UsersController tests

--- apps/runmates-back/src/app/users/users.service.spec.ts
+++ apps/runmates-back/src/app/users/users.service.spec.ts
@@ -1,12 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { mockRepository } from '../../test-utils/mock-repository.util';
+import { AuthService } from '../auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 describe('UsersService', () => {
   let service: UsersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UsersService],
+      providers: [UsersService, AuthService, JwtService, {
+        provide: getRepositoryToken(UserEntity),
+        useValue: mockRepository,
+      }],
     }).compile();
 
     service = module.get<UsersService>(UsersService);
