d4142f8725a02590475b6b70f3284c9258661ed4
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-04-20 22:10:40 -0600

    fix(chat-container): refactor chat message handling and improve structure

--- libs/ui/src/chat/chat-container/chat-container.component.ts
+++ libs/ui/src/chat/chat-container/chat-container.component.ts
@@ -1,8 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, effect, input, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ChatService } from '../chat.service';
 import { RmChatInput } from "../../inputs/chat-input/chat-input.component";
 import { RmMessage } from '@runmates/ui/components';
+import { IonicModule } from '@ionic/angular';
 import { ChatMessage } from '@runmates/types/chats';
 
 export interface ChatUserInfo {
@@ -14,7 +15,7 @@ export interface ChatUserInfo {
 //TODO: Add an animation when a new message is received
 @Component({
   selector: 'rm-chat-container',
-  imports: [CommonModule, RmChatInput, RmMessage],
+  imports: [CommonModule, RmChatInput, RmMessage, IonicModule],
   templateUrl: './chat-container.component.html',
   styleUrl: './chat-container.component.scss',
 })
@@ -23,24 +24,28 @@ export class RmChatContainer {
   public chatId = input<number>();
   public userInfo = input<ChatUserInfo>();
 
-  public messages: any[] = [];
+  public messages = signal<any[]>([]);
+  public displayedMessages = computed(() => this.messages().filter((message) => message.role !== 'system'));
+  
   constructor(private chatService: ChatService) {
     //TODO: THIS MUST BE MOVED outSIDE THE CONSTRUCTOR
     this.chatService.onSignup().subscribe((messages) => {
-      this.messages = messages;
+      this.messages.update(() => messages);
     });
 
     this.chatService.receiveMessage().subscribe((message) => {
 
-      this.messages.push(message);
+      this.messages.update((currentMessages) => [...currentMessages, message]);
     });
   }
 
   public sendMessage(message: string) {
-    this.messages.push({
-      role: 'user',
-      content: message,
-    });
-    this.chatService.sendMessage(this.messages);
+    const newMessage: any = {
+        role: 'user',
+        content: message,
+    };
+    this.messages.update((currentMessages) => [...currentMessages, newMessage]);
+
+    this.chatService.sendMessage(this.messages());
   }
 }
