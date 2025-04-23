2de74bca47edb9c202f59322082c837833195488
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-04-22 22:50:31 -0600

    fix(signup): inject RM_API_CONFIG into AppSignup test setup

--- libs/ui/src/chat/chat.service.spec.ts
+++ libs/ui/src/chat/chat.service.spec.ts
@@ -1,12 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 import { ChatService } from './chat.service';
+import { RM_API_CONFIG } from '../rm.module';
 
 describe('DialogService', () => {
   let service: ChatService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [ChatService],
+      providers: [ChatService, { provide: RM_API_CONFIG, useValue: {} }],
     });
 
     service = TestBed.inject(ChatService);
