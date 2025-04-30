f88b17d73279eb69bdd3580a60469b00120e5858
Ignacio Ambia <ignacio.ambia@gmail.com>
2025-04-29 20:08:37 -0600

    fix(type-orm-naming-strategy): remove debug log from tableName method

--- apps/runmates-back/src/app/type-orm-naming-strategy.ts
+++ apps/runmates-back/src/app/type-orm-naming-strategy.ts
@@ -3,7 +3,6 @@ import { snakeCase } from 'typeorm/util/StringUtils';
 
 export class TypeOrmNamingStrategy extends DefaultNamingStrategy implements NamingStrategyInterface {
   tableName(className: string, customName: string): string {
-    console.log('tableName', className, customName);
     return customName || snakeCase(className.replace('Entity', ''));
   }
 }
