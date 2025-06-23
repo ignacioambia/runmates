7aafcf9eddab6be7f6994ab91dafe2b3ee28d120
Ignacio <ignacio.ambia@gmail.com>
2025-06-23 00:38:19 -0600

    tests(runmates-back): Fix backend tests

--- apps/runmates-back/src/app/chats/chats.service.spec.ts
+++ apps/runmates-back/src/app/chats/chats.service.spec.ts
@@ -4,18 +4,41 @@ import { getRepositoryToken } from '@nestjs/typeorm';
 import { ChatEntity } from './entities/chat.entity';
 import { mockRepository } from '../../test-utils/mock-repository.util';
 import { ConfigService } from '@nestjs/config';
+import { TrainingPlanTemplatesService } from '../training-plan-templates/training-plan-templates.service';
+import { TrainingPlanService } from '../training-plan/training-plan.service';
 
 describe('ChatsService', () => {
   let service: ChatsService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ChatsService, ConfigService,
+      providers: [
+        ChatsService, 
+        ConfigService,
         {
           provide: getRepositoryToken(ChatEntity),
           useValue: mockRepository,
+        },
+        {
+          provide: TrainingPlanTemplatesService,
+          useValue: {
+            findAll: jest.fn(),
+            findOne: jest.fn(),
+            getBestPlan: jest.fn(),
+          },
+        },
+        {
+          provide: TrainingPlanService,
+          useValue: {
+            create: jest.fn(),
+            findAll: jest.fn(),
+          },
         }
       ],
+    }).overrideProvider(ChatsService).useValue({
+      createChat: jest.fn(),
+      processMessage: jest.fn(),
+      startSignupConversation: jest.fn(),
     }).compile();
 
     service = module.get<ChatsService>(ChatsService);
